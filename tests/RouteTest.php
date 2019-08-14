b1a66464389bfdb4ed525134e5d74220a0c6e690
Antti Partanen <aehparta@iki.fi>
2019-08-14 15:28:00 +0300

    fixed index mathing

--- tests/RouteTest.php
+++ tests/RouteTest.php
@@ -9,6 +9,7 @@ final class RouteTest extends PHPUnit\Framework\TestCase
         route_init(__DIR__ . '/test-route.yml');
         $this->assertEmpty(route('/does-not-exist'));
         $this->assertEquals('Index', route_render('/'));
+        $this->assertEquals('Other', route_render('/other'));
         $this->assertEquals('Item 10', route_render('/item/10'));
         $this->assertEquals('Item 10', route_render('/item/1'));
     }
